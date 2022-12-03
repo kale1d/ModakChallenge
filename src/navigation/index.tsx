baa36023af29d7b58cd64ad3ee9a2f401cc0480d
Denise Neustadt <dneustadt@auntap.com>
2022-12-02 21:20:44 -0300

    feat: replace native stack for react native stack and add transition navigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AppStackParamList } from './navTypes';
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from '@react-navigation/stack';
 import { ArtworkDetailScreen, ArtworkListScreen } from '../views';
+import { Dimensions, Platform } from 'react-native';
+const { width, height } = Dimensions.get('screen');
+
+const gestureHandlerConfig = {
+  ...(Platform.OS === 'ios'
+    ? {
+        gestureResponseDistance: width > height ? width : height,
+      }
+    : {}),
+};
 
 export const AppNavigator = () => {
-  const Stack = createNativeStackNavigator<AppStackParamList>();
+  const Stack = createStackNavigator<AppStackParamList>();
   return (
     <NavigationContainer theme={DefaultTheme}>
       <Stack.Navigator
         initialRouteName="ArtworkList"
-        screenOptions={{ headerShown: false }}>
+        screenOptions={{
+          headerShown: false,
+          ...TransitionPresets.ModalSlideFromBottomIOS,
+          ...gestureHandlerConfig,
+        }}>
         <Stack.Screen name="ArtworkList" component={ArtworkListScreen} />
         <Stack.Screen name="ArtworkDetail" component={ArtworkDetailScreen} />
       </Stack.Navigator>
